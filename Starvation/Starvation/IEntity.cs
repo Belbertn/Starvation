cb4b2c5c6f73a635ad2edb3308e7aee0b9b4ec74
Njaal Tangye <belbert95@me.com>
2015-01-26 23:24:05 +0100

    gd

---
+++ Starvation/Starvation/IEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Starvation
+{
+    interface IEntity
+    {
+        Vector2 Postion;
+        void Load();
+        void Update(Starve g, GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
