899a42e7360f02d2cb73be3020b6b1249c7397d1
Njaal Tangye <belbert95@me.com>
2015-01-27 13:11:15 +0100

    dafs

---
+++ Starvation/Starvation/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework.Input;
+
+namespace Starvation
+{
+    class InputManager
+    {
+        public void Update()
+        {
+
+        }
+    }
+}
