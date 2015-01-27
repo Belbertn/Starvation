ba447b5558952b35c7890b933fdd1bc166c86e5e
Njaal Tangye <belbert95@me.com>
2015-01-28 00:07:12 +0100

    Input shiz

--- Starvation/Starvation/InputManager.cs
+++ Starvation/Starvation/InputManager.cs
@@ -9,9 +9,44 @@ namespace Starvation
 {
     class InputManager
     {
+        private Keys[] previousState;
+        private Keys[] currentState;
+
+        public void Initialize()
+        {
+            previousState = new KeyboardState().GetPressedKeys();
+        }
+
         public void Update()
         {
+            currentState = new KeyboardState().GetPressedKeys();
 
+
+            previousState = currentState;
+        }
+
+        public bool IsKeyPressed(Keys key)
+        {
+            if(currentState.Contains<Keys>(key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool WasKeyPressed(Keys key)
+        {
+            if(previousState.Contains<Keys>(key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
