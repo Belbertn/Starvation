620e3ec5c516c79c28e9fee6e51245334b97b740
Njaal Tangye <belbert95@me.com>
2015-01-31 00:04:53 +0100

    AssetManager

---
+++ Starvation/Starvation/Asset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starvation
+{
+    class Asset
+    {
+        public string AssetName { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Lenght { get; set; }
+
+        public Asset(string name, int height, int width, int lenght)
+        {
+            AssetName = name;
+            Height = height;
+            Width = width;
+            Lenght = lenght;
+        }
+    }
+}
---
+++ Starvation/Starvation/AssetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Starvation
+{
+    class AssetManager
+    {
+        private Texture2D spriteSheet;
+
+        public void Load(Starve g)
+        {
+            spriteSheet = g.Content.Load<Texture2D>("spritesheet");
+
+            
+        }
+    }
+}
